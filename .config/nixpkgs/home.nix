99fe32d8535fe9b6a4f94c9317b5d73ed7a0d3ac
aveltras <romain.viallard@outlook.fr>
2021-03-02 15:34:25 +0100

    Tweaks

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -1,43 +1,64 @@
 { config, pkgs, ... }:
 
 {
-  programs.home-manager.enable = true;
-  targets.genericLinux.enable = true;
+  home = {
+    username = "wsl";
+    homeDirectory = "/home/wsl";
+    
+    stateVersion = "21.05";
 
-  home.username = "wsl";
-  home.homeDirectory = "/home/wsl";
+    packages = [
+      pkgs.iosevka-bin
+      pkgs.multimarkdown
+      pkgs.yadm
+    ];
 
-  home.stateVersion = "21.05";
-
-  programs.direnv = {
-    enable = true;
-    enableBashIntegration = true;
-    enableNixDirenvIntegration = true;
-  };
-  
-  programs.emacs.enable = true;
-
-  home.packages = [
-    pkgs.iosevka-bin
-    pkgs.multimarkdown
-    pkgs.yadm
-  ];
-
-  fonts.fontconfig.enable = true;
-
-  programs.git = {
-    enable = true;
-    userEmail = "romain.viallard@outlook.fr";
-    userName = "aveltras";
-    extraConfig = {
-      init.defaultBranch = "main";
-    };
-  };
- 
-  programs.bash = {
-    enable = true;
     sessionVariables = {
       DISPLAY = "$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0";
+      PS1 = " \\[\\033[1;36m\\]\\u\\[\\033[1;31m\\]@\\[\\033[1;32m\\]\\h:\\[\\033[1;35m\\]\\w\\[\\033[1;31m\\]\\$\\[\\033[0m\\] ";
     };
+    
   };
+
+  programs = {
+
+    bash = {
+      enable = true;
+      shellAliases = {
+        ls = "ls --color=auto";
+        grep = "grep --color=auto";
+        fgrep = "fgrep --color=auto";
+        egrep = "egrep --color=auto";
+        ll = "ls -l";
+      };
+    };
+
+    dircolors = {
+      enable = true;
+      enableBashIntegration = true;
+    };
+    
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      enableNixDirenvIntegration = true;
+    };
+
+    emacs.enable = true;
+
+    git = {
+      enable = true;
+      userEmail = "romain.viallard@outlook.fr";
+      userName = "aveltras";
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
+    };
+
+    home-manager.enable = true;
+  };
+  
+  fonts.fontconfig.enable = true;
+
+  targets.genericLinux.enable = true;
 }
